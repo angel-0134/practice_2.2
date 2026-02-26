46fceac46fd7e90498a0f42c3dce1b211f886587
Angel <anzelaisraelan14@gmail.com>
2026-02-26 15:24:09 +0700

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,11 @@
+import requests
+
+# Список сайтов
+urls = [
+    "https://github.com/",
+    "https://www.binance.com/en",
+    "https://tomtit.tomsk.ru/",
+    "https://jsonplaceholder.typicode.com/",
+    "https://moodle.tomtit-tomsk.ru/"
+]
+
